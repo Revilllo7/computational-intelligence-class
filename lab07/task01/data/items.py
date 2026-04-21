78561be228431b51a642babaca26610bcc8648b4
Oliver Gruba <olivergruba7@gmail.com>
2026-04-21 17:02:57 +0200

    chore: lab07 init

---
+++ lab07/task01/data/items.py
@@ -0,0 +1,14 @@
+# id: przedmiot, wartość, waga
+items = { 
+    "item1": {"name": "zegar", "value": 100, "weight": 7.0},
+    "item2": {"name": "obraz-pejzaż", "value": 300, "weight": 7.0},
+    "item3": {"name": "obraz-portret", "value": 200, "weight": 6.0},
+    "item4": {"name": "radio", "value": 40, "weight": 2.0},
+    "item5": {"name": "laptop", "value": 500, "weight": 5.0},
+    "item6": {"name": "lampka nocna", "value": 70, "weight": 6.0},
+    "item7": {"name": "srebrne sztućce", "value": 100, "weight": 1.0},
+    "item8": {"name": "porcelana", "value": 250, "weight": 3.0},
+    "item9": {"name": "figurka z brązu", "value": 300, "weight": 10.0},
+    "item10": {"name": "skórzana torebka", "value": 280, "weight": 3.0},
+    "item11": {"name": "odkurzacz", "value": 300, "weight": 15.0}
+}
---
+++ lab07/task01/task01_report.md
@@ -0,0 +1,1 @@
+# Task 01: Knapsack Problem (Thief Problem)
